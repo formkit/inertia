d4799a69cabe6d4da8a94558e2f0f708b6a8117e
Gustavo Fenilli <gustavofenilli@gmail.com>
2023-10-30 17:08:55 -0300

    build: fixes external dependency bundles

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  external: ['@formkit/theme-creator'],
   sourcemap: true,
   clean: true,
   dts: true,
